22ab6c752d14d26aba60eae0c3405af0e5fc6539
k4nl <gustavoh.bh@gmail.com>
2021-11-24 03:45:59 -0300

    ajuste header

--- azapfy-hero/src/components/Header.jsx
+++ azapfy-hero/src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../images/logo.png'
 
 const Header = () => {
   return (
-    <header className="d-flex justify-content-center">
+    <header className="d-flex justify-content-start ms-4">
       <div className="d-flex align-items-center">
         <img src={logo} alt="azapfy-logo"></img>
       </div>
